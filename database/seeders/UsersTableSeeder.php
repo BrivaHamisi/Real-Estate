c262ab86f639a35ea5c41ca8eca73a3db1418ed1
BrivaHamisi <brivahamisi@gmail.com>
2024-01-10 19:18:01 +0300

    Seeders Working

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -16,32 +16,36 @@ class UsersTableSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            //Admin
+            //  Admin
+
             [
                 'name' => 'Admin',
                 'username' => 'admin',
-                'email'=>'admin@gmail.com',
-                'password' =>Hash::make('111'),
-                'role' => 'active',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'admin',
+                'status' => 'active',
             ],
 
-            //Agent
+            //  Agent
             [
                 'name' => 'Agent',
                 'username' => 'agent',
-                'email'=>'agent@gmail.com',
-                'password' =>Hash::make('111'),
-                'role' => 'active',
+                'email' => 'agent@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'agent',
+                'status' => 'active',
             ],
-
-            //User
+            //  User
             [
                 'name' => 'User',
                 'username' => 'user',
-                'email'=>'user@gmail.com',
-                'password' =>Hash::make('111'),
-                'role' => 'active',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'user',
+                'status' => 'active',
             ],
+
 
         ]);
     }
